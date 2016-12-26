fda42965c3346e2446673c771321a71262125d81
Basil Khan <basilkhan.dev@gmail.com>
2016-12-25 19:30:01 -0500

    feat(addProduct): Add Product and Store functionality

--- api.py
+++ api.py
@@ -5,37 +5,37 @@ from flask_pymongo import PyMongo
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = "students_db"
+app.config["MONGO_DBNAME"] = "products_db"
 mongo = PyMongo(app, config_prefix='MONGO')
 APP_URL = "http://127.0.0.1:5000"
 
 
-class Student(Resource):
-    def get(self, registration=None, department=None):
+class Product(Resource):
+    def get(self, product_id=None, store=None):
         data = []
 
-        if registration:
-            studnet_info = mongo.db.student.find_one({"registration": registration}, {"_id": 0})
-            if studnet_info:
-                return jsonify({"status": "ok", "data": studnet_info})
+        if product_id:
+            store_info = mongo.db.product.find_one({"product_id": product_id}, {"_id": 0})
+            if store_info:
+                return jsonify({"status": "ok", "data": store_info})
             else:
-                return {"response": "no student found for {}".format(registration)}
+                return {"response": "no product found for {}".format(product_id)}
 
-        elif department:
-            cursor = mongo.db.student.find({"department": department}, {"_id": 0}).limit(10)
-            for student in cursor:
-                student['url'] = APP_URL + url_for('students') + "/" + student.get('registration')
-                data.append(student)
+        elif store:
+            cursor = mongo.db.product.find({"store": store}, {"_id": 0}).limit(10)
+            for product in cursor:
+                product['url'] = APP_URL + url_for('products') + "/" + product.get('product_id')
+                data.append(product)
 
-            return jsonify({"department": department, "response": data})
+            return jsonify({"store": store, "products": data})
 
         else:
-            cursor = mongo.db.student.find({}, {"_id": 0, "update_time": 0}).limit(10)
+            cursor = mongo.db.product.find({}, {"_id": 0, "update_time": 0}).limit(10)
 
-            for student in cursor:
-                print student
-                student['url'] = APP_URL + url_for('students') + "/" + student.get('registration')
-                data.append(student)
+            for product in cursor:
+                print product
+                product['url'] = APP_URL + url_for('products') + "/" + product.get('product_id')
+                data.append(product)
 
             return jsonify({"response": data})
 
@@ -45,37 +45,37 @@ class Student(Resource):
             data = {"response": "ERROR"}
             return jsonify(data)
         else:
-            registration = data.get('registration')
-            if registration:
-                if mongo.db.student.find_one({"registration": registration}):
-                    return {"response": "student already exists."}
+            product_id = data.get('product_id')
+            if product_id:
+                if mongo.db.product.find_one({"product_id": product_id}):
+                    return {"response": "product already exists."}
                 else:
-                    mongo.db.student.insert(data)
+                    mongo.db.product.insert(data)
             else:
-                return {"response": "registration number missing"}
+                return {"response": "product_id number missing"}
 
-        return redirect(url_for("students"))
+        return redirect(url_for("products"))
 
-    def put(self, registration):
+    def put(self, product_id):
         data = request.get_json()
-        mongo.db.student.update({'registration': registration}, {'$set': data})
-        return redirect(url_for("students"))
+        mongo.db.product.update({'product_id': product_id}, {'$set': data})
+        return redirect(url_for("products"))
 
-    def delete(self, registration):
-        mongo.db.student.remove({'registration': registration})
-        return redirect(url_for("students"))
+    def delete(self, product_id):
+        mongo.db.product.remove({'product_id': product_id})
+        return redirect(url_for("products"))
 
 
 class Index(Resource):
     def get(self):
-        return redirect(url_for("students"))
+        return redirect(url_for("products"))
 
 
 api = Api(app)
 api.add_resource(Index, "/", endpoint="index")
-api.add_resource(Student, "/api/", endpoint="students")
-api.add_resource(Student, "/api/<string:registration>", endpoint="registration")
-api.add_resource(Student, "/api/department/<string:department>", endpoint="department")
+api.add_resource(Product, "/api/", endpoint="products")
+api.add_resource(Product, "/api/<string:product_id>", endpoint="product_id")
+api.add_resource(Product, "/api/store/<string:store>", endpoint="store")
 
 if __name__ == "__main__":
     app.run(debug=True)
