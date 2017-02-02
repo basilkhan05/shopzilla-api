5329b940f866fd455b32eacea2f8a143272032b2
Basil Khan <basilkhan.dev@gmail.com>
2017-02-01 23:52:11 -0500

    fix(idErrors): Follow shopify json template for standardized json

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from flask_pymongo import PyMongo
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = "products_db"
+app.config["MONGO_DBNAME"] = "shopzilla"
 mongo = PyMongo(app, config_prefix='MONGO')
 APP_URL = "http://127.0.0.1:5000"
 
@@ -15,7 +15,7 @@ class Product(Resource):
         data = []
 
         if product_id:
-            store_info = mongo.db.product.find_one({"product_id": product_id}, {"_id": 0})
+            store_info = mongo.db.product.find_one({"id": product_id}, {"_id": 0})
             if store_info:
                 return jsonify({"status": "ok", "data": store_info})
             else:
@@ -24,7 +24,7 @@ class Product(Resource):
         elif store:
             cursor = mongo.db.product.find({"store": store}, {"_id": 0}).limit(10)
             for product in cursor:
-                product['url'] = APP_URL + url_for('products') + "/" + product.get('product_id')
+                product['url'] = APP_URL + url_for('products') + "/" + product.id.get('product_id')
                 data.append(product)
 
             return jsonify({"store": store, "products": data})
@@ -34,7 +34,7 @@ class Product(Resource):
 
             for product in cursor:
                 print(product)
-                product['url'] = APP_URL + url_for('products') + "/" + product.get('product_id')
+                product['url'] = APP_URL + url_for('products') + "/" + str(product.get('id'))
                 data.append(product)
 
             return jsonify({"response": data})
@@ -45,20 +45,20 @@ class Product(Resource):
             data = {"response": "ERROR"}
             return jsonify(data)
         else:
-            product_id = data.get('product_id')
+            product_id = data.get('id')
             if product_id:
-                if mongo.db.product.find_one({"product_id": product_id}):
+                if mongo.db.product.find_one({"id": product_id}):
                     return {"response": "product already exists."}
                 else:
                     mongo.db.product.insert(data)
             else:
-                return {"response": "product_id number missing"}
+                return {"response": "product id number is missing"}
 
         return redirect(url_for("products"))
 
     def put(self, product_id):
         data = request.get_json()
-        mongo.db.product.update({'product_id': product_id}, {'$set': data})
+        mongo.db.product.update({'id': product_id}, {'$set': data})
         return redirect(url_for("products"))
 
     def delete(self, product_id):
@@ -73,7 +73,7 @@ class Index(Resource):
 
 api = Api(app)
 api.add_resource(Index, "/", endpoint="index")
-api.add_resource(Product, "/api/", endpoint="products")
+api.add_resource(Product, "/api", endpoint="products")
 api.add_resource(Product, "/api/<string:product_id>", endpoint="product_id")
 api.add_resource(Product, "/api/store/<string:store>", endpoint="store")
 
