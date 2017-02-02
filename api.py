c893e98f298c46d8e64a8115a4ae365996f106c9
Basil Khan <basilkhan.dev@gmail.com>
2017-02-02 00:35:25 -0500

    feat(limit): Add record limit for Index call
    - allow limit argument with default of 10
    - Specify /api?limit=int in GET for int records

--- api.py
+++ api.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, jsonify, url_for, redirect, request
 from flask_pymongo import PyMongo
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "shopzilla"
@@ -13,6 +13,12 @@ APP_URL = "http://127.0.0.1:5000"
 class Product(Resource):
     def get(self, product_id=None, store=None):
         data = []
+        URLparser = reqparse.RequestParser()
+        URLparser.add_argument('limit', type=str)
+        args = URLparser.parse_args()
+        limit = int(args['limit']) if args['limit'] else 10 
+
+        # print('----------------HELLO -------------' + parser.parse_args())
 
         if product_id:
             store_info = mongo.db.product.find_one({"id": int(product_id)}, {"_id": 0})
@@ -22,7 +28,7 @@ class Product(Resource):
                 return {"response": "no product found for {}".format(product_id)}
 
         elif store:
-            cursor = mongo.db.product.find({"store": store}, {"_id": 0}).limit(10)
+            cursor = mongo.db.product.find({"store": store}, {"_id": 0}).limit(limit)
             for product in cursor:
                 product['url'] = APP_URL + url_for('products') + "/" + str(product.get('id'))
                 data.append(product)
@@ -30,7 +36,7 @@ class Product(Resource):
             return jsonify({"store": store, "products": data})
 
         else:
-            cursor = mongo.db.product.find({}, {"_id": 0, "update_time": 0}).limit(10)
+            cursor = mongo.db.product.find({}, {"_id": 0, "update_time": 0}).limit(limit)
 
             for product in cursor:
                 print(product)
@@ -74,7 +80,7 @@ class Index(Resource):
 api = Api(app)
 api.add_resource(Index, "/", endpoint="index")
 api.add_resource(Product, "/api", endpoint="products")
-api.add_resource(Product, "/api/<string:product_id>", endpoint="product_id")
+api.add_resource(Product, "/api/product/<string:product_id>", endpoint="product_id")
 api.add_resource(Product, "/api/store/<string:store>", endpoint="store")
 
 if __name__ == "__main__":
